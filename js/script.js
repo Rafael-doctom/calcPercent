a52c1eac4150e5d21dcbf5afe48bb653e42c08ef
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2021-12-04 14:46:06 -0300

    feat: completed Calc

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function resultPercent(){
+    var value1 = Number(document.getElementById("n1").value)
+    var value2 = Number(document.getElementById("n2").value)
+    var resultSum = (value1/100)*value2
+    alert(resultSum)
+}
